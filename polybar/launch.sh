b77572baa57fe2c27190a1357852e6e6acbf3ae1
dheerajkrishna141 <dheerajkrishna141@gmail.com>
2026-03-15 19:07:16 -0400

    Polybar on all monitors:
    - Updated polybar config to display polybar on all connected monitors.

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -4,6 +4,12 @@ killall -q polybar
 
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-polybar -rq top &
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar -rq top  &
+  done
+else
+  polybar -rq top &
+fi
 
 echo "Polybar launched..."
